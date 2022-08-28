91c752d5d4e4b0f9a6937cd2326c61f83aa63630
kkrivitskaja <pratugames@gmail.com>
2022-08-28 11:11:03 +0300

    feat: added PostCard component

--- src/components/PostCard/UpvoteIcon.js
+++ src/components/PostCard/UpvoteIcon.js
@@ -1,10 +1,18 @@
-import style from '../../styles/Home.module.scss';
+import style from '../../../styles/Home.module.scss';
 
 const UpvoteIcon = () => {
     return (
-        <div className={style['upvote']}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#808080F" viewBox="0 0 16 16">
-                <path d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
+        <div className={style['icon']}>
+            <svg
+                className="w-5 text-[#818384]"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 24 24"
+            >
+                <path
+                    fill="currentColor"
+                    d="M12.781 2.375c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10zM15 12h-1v8h-4v-8H6.081L12 4.601L17.919 12H15z"
+                ></path>
             </svg>
         </div>
     );
--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,27 +1,34 @@
 import { useMutation } from '@apollo/client';
+import Link from 'next/link';
 
 import { VOTE_FOR_LINK } from '../../graphql/Queries';
-import UpvoteIcon from './UpvoteIcon';
-import VotesInfo from './VotesInfo';
 
-import style from '../../styles/Home.module.scss';
+import VotesInfo from './VotesInfo';
+import Vote from './Vote';
+
+import style from '../../../styles/Home.module.scss';
 
 const PostCard = ({ post }) => {
-    const upvoteHandler = () =>{
-        useMutation(VOTE_FOR_LINK, {
-            variables: { linkId: post.id },
-        })
-            .then(() => alert('Upvoted!'))
-            .catch(() => alert('You already voted!'));}
-    
+    // const upvoteHandler = () => {
+    //     useMutation(VOTE_FOR_LINK, {
+    //         variables: { linkId: post.id },
+    //     })
+    //         .then(() => alert('Upvoted!'))
+    //         .catch(() => alert('You already voted!'));
+    // };
+
     return (
         <div className={style['card']}>
-            <div>
-                <h3>{post.description}</h3>
-            </div>
             <div className={style['card-vote']}>
-                <UpvoteIcon onClick={upvoteHandler} />
-                <VotesInfo votes={post.votes} title={post.description} />
+                <Vote linkId={post.id} />
+                <div className={style['card-context']}>
+                    <h3>
+                        <Link href={post.url}>
+                            <a target="_blank">{post.description}</a>
+                        </Link>
+                    </h3>
+                    <VotesInfo votes={post.votes} title={post.description} />
+                </div>
             </div>
         </div>
     );
