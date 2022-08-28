c612366dccc33a6821060de0dcb5c686b0ecf043
kkrivitskaja <pratugames@gmail.com>
2022-08-28 10:56:03 +0300

    feat: added graphql quert and mutation

--- graphql/Queries.js
+++ graphql/Queries.js
@@ -23,3 +23,33 @@ export const GET_ALL_LINKS = gql`
         }
     }
 `;
+
+export const GET_SINGLE_POST_INFO = gql`
+    query getPost($linkId: ID!) {
+        links(linkId: $linkId) {
+            id
+            description
+            url
+            postedBy {
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                    id
+                    name
+                }
+            }
+            comments {
+                id
+                user {
+                    id
+                    name
+                }
+                post_id
+                text
+            }
+        }
+    }
+`;
